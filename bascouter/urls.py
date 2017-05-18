7df39889085c9f22a036cacdb8543dedc7ceac26
aamjX <antmunjim2@alum.us.es>
2017-05-18 14:29:48 -0700

    Política de cookies y términos y condiciones

--- bascouter/urls.py
+++ bascouter/urls.py
@@ -6,6 +6,7 @@ from django.contrib.auth.views import  password_reset, password_reset_done, \
     password_reset_confirm, password_reset_complete
 from apps.usuario.views import index_view
 from django.conf import settings
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
@@ -26,6 +27,8 @@ urlpatterns = [
         ),
     url(r'^reset/done', password_reset_complete, {'template_name': 'reset_password/password_reset_complete.html'},
         name='password_reset_complete'),
+    url(r'^cookies_directions',  TemplateView.as_view(template_name='law/cookies_directions.html'), name='cookies_directions'),
+    url(r'^terms_and_conditions',  TemplateView.as_view(template_name='law/terms_and_conditions.html'), name='terms_and_conditions'),
 ]
 
 if settings.DEBUG:
